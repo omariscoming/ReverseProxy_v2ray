3195dadf8a0ec2b0f2560cd8a41dc9a5a69d875a
Omar <31930599+omariscoming@users.noreply.github.com>
2023-02-05 14:59:35 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ echo "ln the $DOMAIN ..."
 echo "edit the file..."
 
 sed -i "s/_;/$DOMAIN;/" "/etc/nginx/sites-available/$DOMAIN"
-cp ReverseProxy_x-ui/reverse.txt ReverseProxy_x-ui/newreverse.txt
-sed -i "21 r newreverse.txt" "/etc/nginx/sites-available/$DOMAIN"
+sed -i "s/ default_server//" "/etc/nginx/sites-available/$DOMAIN"
+sed -i "21 r reverse.txt" "/etc/nginx/sites-available/$DOMAIN"
 
 certbot --nginx -d $DOMAIN --register-unsafely-without-email
 
