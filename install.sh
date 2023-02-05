3692dcec5f6465fb3fa760aeb3b353f203c08512
Omar <31930599+omariscoming@users.noreply.github.com>
2023-02-05 13:21:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,27 +2,30 @@ apt-get update -y && apt-get upgrade -y
 sudo apt install nginx certbot python3-certbot-nginx -y
 
 read -p "Enter Your Domain: " DOMAIN
+read -p "Enter your Subdomain: " SUBDOMAIN
 mkdomain(){
-cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$DOMAIN
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$SUBDOMAIN.$DOMAIN
 }
 mkdomain
+echo "copying..."
 
 lndomain(){
-ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/$SUBDOMAIN.$DOMAIN /etc/nginx/sites-enabled/
 }
+
 lndomain
+echo "ln the $SUBDOMAIN.$DOMAIN ..."
 
 
 echo "edit the file..."
 
-sed -i "s/_;/$DOMAIN;/" "/etc/nginx/sites-available/$DOMAIN"
-sed -i "s/ default_server//" "/etc/nginx/sites-available/$DOMAIN"
-sed -i "21 r reverseproxy.txt" "/etc/nginx/sites-available/$DOMAIN"
+sed -i "s/_;/$SUBDOMAIN.$DOMAIN;/" "/etc/nginx/sites-available/$SUBDOMAIN.$DOMAIN"
+sed -i "s/ default_server//" "/etc/nginx/sites-available/$SUBDOMAIN.$DOMAIN"
+sed -i "21 r reverseproxy.txt" "/etc/nginx/sites-available/$SUBDOMAIN.$DOMAIN"
 
-certbot --nginx -d $DOMAIN --register-unsafely-without-email
+certbot --nginx -d $SUBDOMAIN.$DOMAIN --register-unsafely-without-email
 
 systemctl restart nginx
 apt install curl
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 wget https://github.com/Heclalava/blockpublictorrent-iptables/raw/main/bt.sh && chmod +x bt.sh && bash bt.sh
-
