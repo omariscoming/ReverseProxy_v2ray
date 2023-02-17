552f909996e1125bb83429e726f25499f994d7d3
Omar <31930599+omariscoming@users.noreply.github.com>
2023-02-18 00:09:01 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,29 +3,15 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
-installnginx(){
-sudo apt install nginx certbot python3-certbot-nginx -y
-}
-
 mkdomain(){
+sudo apt install nginx certbot python3-certbot-nginx -y
 read -p "Enter Your Domain: " DOMAIN
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$DOMAIN
-}
-lndomain(){
 ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
-}
-
-copying(){
 sed -i "s/_;/$DOMAIN;/" "/etc/nginx/sites-available/$DOMAIN"
 sed -i "s/ default_server//" "/etc/nginx/sites-available/$DOMAIN"
 sed -i "21 r /root/ReverseProxy_v2ray/reverse.txt" "/etc/nginx/sites-available/$DOMAIN"
-}
-
-certbot(){
 certbot --nginx -d $DOMAIN --register-unsafely-without-email
-}
-
-restartnginx(){
 systemctl restart nginx
 }
 
@@ -40,19 +26,17 @@ read -p "USERNAME: " USERNAME
 scp $USERNAME@$OLDONE:/etc/x-ui/x-ui.db /etc/x-ui/
 }
 
-firstoption(){
-installnginx
-mkdomain
-lndomain
-copying
-certbot
-restartnginx
-}
-
-secondoption(){
-firstoption
-xuiinstall
-}
-
-firstoption
-
+echo -e "${RED}1_install Nginx\n${GREEN}2_install Nginx + x-ui\n${YELLOW}3_Backup\n\n${NOCOLOR}chose an option:"
+read VAR
+case $VAR in
+    1)
+        mkdomain
+        ;;
+    2)
+        mkdomain
+        xuiinstall
+        ;;
+    3)
+        backup
+        ;;
+    esac
